850b28e82031603fd783eda84cd205e0224b410c
Pritesh Mantri <priteshpmantri@gmail.com>
2021-09-08 20:34:47 +0530

    Added psudo login

--- src/index.ts
+++ src/index.ts
@@ -5,32 +5,53 @@ import { Socket } from "socket.io";
 import path from "path";
 
 const main = () => {
-    const app = express();
+	const app = express();
 
-    app.use(cors());
+	app.use(cors({ credentials: true, origin: "http://localhost:3000" }));
 
-    app.use(express.json());
-    app.set("view engine", "ejs");
-    app.set("views", path.join(__dirname, "views"));
-    app.use(express.static(path.join(__dirname, "/public/")));
+	app.use(express.json());
+	app.set("view engine", "ejs");
+	app.set("views", path.join(__dirname, "views"));
+	app.use(express.static(path.join(__dirname, "/public/")));
 
-    app.get("/", (req, res) => {
-        return res.render("index");
-    });
+	app.get("/", (req, res) => {
+		return res.render("index");
+	});
 
-    const server = new Server(app);
-    const io = require("socket.io")(server);
+	let activeUsers: { userId: string; userName: string }[] = [];
+	const server = new Server(app);
+	const io = require("socket.io")(server);
 
-    io.on("connection", (socket: Socket) => {
-        console.log("Client connected");
-        socket.on("message", msg => {
-            console.log(msg);
-        });
-    });
+	io.on("connection", (socket: Socket) => {
+		console.log("Client connected");
 
-    server.listen(5000, () => {
-        console.log("Listening on port 5000");
-    });
+		socket.on("login", (userName: string) => {
+			activeUsers.push({ userName, userId: socket.id });
+			io.emit("activeUsers", activeUsers);
+		});
+
+		socket.on("move", (move) => {
+			socket.to("roomName").emit("move", move);
+		});
+
+		socket.on("joinGame", () => {
+			console.log("room joined");
+			socket.join("roomName");
+		});
+
+		socket.on("roomEvent", () => {
+			io.to("roomName").emit("roomEvent", "Hello There");
+		});
+
+		socket.on("disconnect", () => {
+			activeUsers = activeUsers.filter((user) => user.userId !== socket.id);
+			io.emit("activeUsers", activeUsers);
+		});
+	});
+
+	server.listen(5000, () => {
+		console.log("Listening on port 5000");
+	});
 };
 
 main();
--- src/public/js/index.js
+++ src/public/js/index.js
@@ -1,17 +1,57 @@
-var game;
-var board;
+let client = io.connect();
+let game;
+let board;
 
-window.onload = function () {
-	initGame();
-};
-
-function initGame() {
-	const config = { draggable: true, position: "start", onDrop: handleMoves };
+function initGame(data) {
+	const config = {
+		draggable: true,
+		position: "start",
+		onDrop: handleMoves,
+		orientation: data.color,
+	};
 	board = new ChessBoard("board", config);
 	game = new Chess();
 }
 
 function handleMoves(from, to) {
 	const move = game.move({ from, to });
-	if (move === null) return "snapback";
+	if (move === null) {
+		return "snapback";
+	} else {
+		client.emit("move", move);
+	}
+	if (game.game_over()) {
+		let winner;
+		if (game.in_checkmate()) {
+			winner = move.color === "w" ? "White Won" : "Black Won";
+		} else if (
+			game.in_draw() ||
+			game.in_stalemate() ||
+			game.in_threefold_repetition()
+		) {
+			winner = "Game Draw";
+		}
+		$("#result").text(winner);
+	}
 }
+
+client.on("move", (move) => {
+	console.log(move);
+	game.move(move);
+	board.position(game.fen());
+});
+
+client.on("joinGame", (data) => initGame(data));
+client.on("roomEvent", (data) => console.log(data));
+client.on("activeUsers", (data) => console.log(data));
+
+//Login
+$("#login-form").on("submit", (e) => {
+	e.preventDefault();
+	const userName = $("#username").val();
+	client.emit("login", userName);
+	console.log("Sent request");
+	$("#login-page").hide();
+	$("#home-page").show();
+	$("#title-name").text(userName)
+});
